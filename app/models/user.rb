a28aa20f6194e7afffd1c2a71df38044b1b91f96
siraitooru <zabiisann@gmail.com>
2020-07-23 16:52:13 +0000

    fix code

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,12 +35,6 @@ class User < ApplicationRecord
     BCrypt::Password.new(digest).is_password?(token)
   end
 
-  # 渡されたトークンがダイジェストと一致したらtrueを返す
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
-  end
-
   # ユーザーのログイン情報を破棄する
   def forget
     update_attribute(:remember_digest, nil)
